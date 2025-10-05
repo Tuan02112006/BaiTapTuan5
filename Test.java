776ece24ebda8b84eeb399b08720a2a3dcf238ef
Tuan02112006 <nguyenanhtuan021106@gmail.com>
2025-10-05 17:57:52 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số lượng sách cần thêm vào thư viện: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        Sach[] danhSach = new Sach[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nNhập thông tin sách thứ " + (i + 1) + ":");
+            System.out.print("Mã sách: ");
+            String maSach = sc.nextLine();
+
+            System.out.print("Tiêu đề: ");
+            String tieuDe = sc.nextLine();
+
+            System.out.print("Tác giả: ");
+            String tacGia = sc.nextLine();
+
+            System.out.print("Năm xuất bản: ");
+            int namXuatBan = sc.nextInt();
+
+            System.out.print("Số lượng: ");
+            int soLuong = sc.nextInt();
+            sc.nextLine();
+
+            danhSach[i] = new Sach(maSach, tieuDe, tacGia, namXuatBan, soLuong);
+        }
+
+        System.out.println("\n===== DANH SÁCH SÁCH TRONG THƯ VIỆN =====");
+        for (int i = 0; i < n; i++) {
+            danhSach[i].hienThiThongTin();
+        }
+    }
+}
