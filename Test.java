f69180a6c62d468f55a849518f3380b808828386
phudong26-coder <toannmai22@gmail.com>
2025-10-14 19:43:54 +0700

    capnhat

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        Sach s1 = new Sach("S01", "Lập trình Java", "Nguyễn Văn A", 2023, 5);
+        s1.hienThiThongTin();
+
+        System.out.println("----------------------");
+
+        Sach s2 = new Sach("S02", "Mưa đỏ", "Chu Lai", 1975, 5);
+        s2.hienThiThongTin();
+    }
+}
+}
