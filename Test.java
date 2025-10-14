50f37a3ec4c0c70a75bfaf19fc3b635b5527068e
NguyenTrongVu2006 <trongvu670@gmail.com>
2025-10-14 19:54:06 +0700

    BtapTuan6

--- Test.java
+++ Test.java
@@ -1,17 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        // Tạo sách bằng constructor đầy đủ
-        Sach sach1 = new Sach("S001", "Lập trình Java cơ bản",
-                "Nguyễn Văn A", 2023, 10);
-        // Tạo sách bằng constructor mặc định + setter
-        Sach sach2 = new Sach();
-        sach2.setMaSach("S002");
-        sach2.setTieuDe("Cấu trúc dữ liệu & Giải thuật");
-        sach2.setTacGia("Trần Văn B");
-        sach2.setNamXuatBan(2021);
-        sach2.setSoLuong(5);
-        sach1.hienThiThongTin();
-        System.out.println("----------------------");
-        sach2.hienThiThongTin();
+        QuanLySach ql = new QuanLySach();
+        ql.menu();
     }
 }
