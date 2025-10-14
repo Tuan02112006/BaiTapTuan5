423e371ec64ff4f4074026faa5ab2c62acc7a9c8
phudong26-coder <toannmai22@gmail.com>
2025-10-14 19:51:52 +0700

    capnhatbaitap

--- Test.java
+++ Test.java
@@ -1,12 +1,32 @@
 public class Test {
     public static void main(String[] args) {
+        QuanLySach ql = new QuanLySach();
+
+        // Thêm sách thường
         Sach s1 = new Sach("S01", "Lập trình Java", "Nguyễn Văn A", 2023, 5);
-        s1.hienThiThongTin();
+        ql.themSach(s1);
 
-        System.out.println("----------------------");
+        // Thêm sách giáo trình (lớp con)
+        SachGiaoTrinh g1 = new SachGiaoTrinh("G01", "Toán rời rạc", "Trần Thị B", 2020, 10, "Toán", "Đại học");
+        ql.themSach(g1);
 
-        Sach s2 = new Sach("S02", "Mưa đỏ", "Chu Lai", 1975, 5);
-        s2.hienThiThongTin();
+        // Thêm sách tiểu thuyết (lớp con)
+        SachTieuThuyet t1 = new SachTieuThuyet("T01", "Hoàng tử bé", "Saint-Exupéry", 1943, 7, "Truyện thiếu nhi", false);
+        ql.themSach(t1);
+
+        System.out.println("\n=== Danh sách sách hiện có ===");
+        ql.hienThiDanhSach();
+
+        System.out.println("\n=== Tìm sách có mã G01 ===");
+        ql.timKiemVaIn("G01");
+
+        System.out.println("\n=== Cập nhật số lượng sách S01 ===");
+        ql.capNhatSoLuong("S01", 15);
+
+        System.out.println("\n=== Xóa sách mã T01 ===");
+        ql.xoaSach("T01");
+
+        System.out.println("\n=== Danh sách sau khi xóa ===");
+        ql.hienThiDanhSach();
     }
-}
 }
