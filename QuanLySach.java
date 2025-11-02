c274d54f373c9ef540cf23e9093b671586c81ebe
Tuan02112006 <nguyenanhtuan021106@gmail.com>
2025-11-02 15:21:56 +0700

    Add files via upload

--- QuanLySach.java
+++ QuanLySach.java
@@ -1,80 +1,42 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class QuanLySach {
-    private Sach[] danhSachSach;
-    private int soLuong; // số lượng sách hiện có
+public class QuanLySach implements IQuanLySach {
+    private List<Sach> danhSach = new ArrayList<>();
 
-    public QuanLySach(int kichThuocToiDa) {
-        danhSachSach = new Sach[kichThuocToiDa];
-        soLuong = 0;
+    @Override
+    public void themSach(Sach sach) {
+        danhSach.add(sach);
     }
 
-    // Thêm sách
-    public boolean themSach(Sach sach) {
-        if (soLuong >= danhSachSach.length) {
-            System.out.println("Danh sach da day, khong the them sach moi!");
-            return false;
-        }
-        danhSachSach[soLuong] = sach;
-        soLuong++;
-        return true;
-    }
-
-    // Tìm sách theo mã
-    public Sach timSachTheoMa(String maSach) {
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSachSach[i].getMaSach().equalsIgnoreCase(maSach)) {
-                return danhSachSach[i];
+    @Override
+    public Sach timKiemSach(String maSach) {
+        for (Sach s : danhSach) {
+            if (s.getMaSach().equalsIgnoreCase(maSach)) {
+                return s;
             }
         }
         return null;
     }
 
-    // Xóa sách theo mã
+    @Override
     public boolean xoaSach(String maSach) {
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSachSach[i].getMaSach().equalsIgnoreCase(maSach)) {
-                // Dịch các phần tử phía sau lên 1 vị trí
-                for (int j = i; j < soLuong - 1; j++) {
-                    danhSachSach[j] = danhSachSach[j + 1];
-                }
-                danhSachSach[soLuong - 1] = null;
-                soLuong--;
-                return true;
-            }
+        Sach sach = timKiemSach(maSach);
+        if (sach != null) {
+            danhSach.remove(sach);
+            return true;
         }
         return false;
     }
 
-    public boolean capNhatSach(String maSach, Sach sachMoi) {
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSachSach[i].getMaSach().equalsIgnoreCase(maSach)) {
-                danhSachSach[i].setTieuDe(sachMoi.getTieuDe());
-                danhSachSach[i].setTacGia(sachMoi.getTacGia());
-                danhSachSach[i].setNamXuatBan(sachMoi.getNamXuatBan());
-                danhSachSach[i].setSoLuong(sachMoi.getSoLuong());
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Hiển thị danh sách sách
+    @Override
     public void hienThiDanhSach() {
-        if (soLuong == 0) {
-            System.out.println("Danh sách sách trống!");
-            return;
+        if (danhSach.isEmpty()) {
+            System.out.println("Danh sách trống!");
+        } else {
+            for (Sach s : danhSach) {
+                System.out.println(s);
+            }
         }
-        for (int i = 0; i < soLuong; i++) {
-            System.out.println(danhSachSach[i]);
-            System.out.println("→ Giá bán ước tính: " + danhSachSach[i].tinhGiaBan() + " VNĐ");
-            System.out.println("-----------------------------");
-        }
-    }
-
-
-    // Lấy số lượng sách hiện có
-    public int getSoLuong() {
-        return soLuong;
     }
 }
